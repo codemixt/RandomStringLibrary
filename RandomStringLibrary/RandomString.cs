65487a3ccd96cdaefce80d8b362fa21af567a969
ahmed sobh <ahmed sobh@DESKTOP-ENPNO4D>
2023-06-22 22:07:36 +0300

    Add project files.

---
+++ RandomStringLibrary/RandomString.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomStringLibrary
+{
+    public class RandomString
+    {
+        // List of characters and numbers to be used...  
+        private static List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+        private static List<char> characters = new List<char>()
+            {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
+            'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+            'Q', 'R', 'S',  'T', 'U', 'V', 'W', 'X', 'Y', 'Z',};
+
+        public static string GetString(int length)
+        {
+            string str = "";
+            Random rand = new Random();
+            // run the loop till I get a string of 10 characters  
+            for (int i = 0; i < length; i++)
+            {
+                // Get random numbers, to get either a character or a number...  
+                int random = rand.Next(0, 3);
+                if (random == 1)
+                {
+                    // use a number  
+                    random = rand.Next(0, numbers.Count);
+                    str += numbers[random].ToString();
+                }
+                else
+                {
+                    random = rand.Next(0, characters.Count);
+                    str += characters[random].ToString();
+                }
+            }
+            return str;
+        }
+    }
+}
